44a25bcae8cf16a7fa3348da58fecaa04f40d389
Tom Jager <tj15299@my.bristol.ac.uk>
2017-09-08 15:34:57 +0200

    Completed drawing tool and hilbert

--- Curves.hs
+++ Curves.hs
@@ -1,8 +1,9 @@
-module Curves where
+module Curves () where
+import Text.Printf (printf)
 
 data Point = Point Double Double deriving (Show)
--- point :: (Double, Double) -> Point
--- point (x,y) = Point x y
+point :: (Double, Double) -> Point
+point (x,y) = Point x y
 
 pointX :: Point -> Double
 pointX (Point x _) = x
@@ -16,6 +17,9 @@ instance Eq Point where
        | otherwise                                = False
 
 data Curve = Curve Point [Point] deriving (Show)
+curve :: Point -> [Point] -> Curve
+curve h t = Curve h t
+
 connect :: Curve -> Curve -> Curve
 connect (Curve h1 t1) (Curve h2 t2) = Curve h1 (t1 ++ [h2] ++ t2)
 
@@ -94,8 +98,40 @@ normalize (Curve h t) = translate (Curve h t) (Point dx dy) where
     corner = fst $ bbox (Curve h t)
     dx = pointX h - pointX corner
     dy = pointY h - pointY corner
-    
+
 
 testCurve1, testCurve2 :: Curve
 testCurve1 = Curve (Point 3.0 5.0) [(Point 6.0 4.0), (Point 1 2)]
 testCurve2 = rotate testCurve1 90.0
+
+toSVG ::Curve -> String
+toSVG (Curve h t) = svgString where
+  l            = h:t
+  svgString    = (printf "<svg xmlns=\"http://www.w3.org/2000/svg\"\n\twidth=\"%dpx\" height=\"%dpx\" version=\"1.1\">\n<g>" ((ceiling (width (Curve h t)))::Int) ((ceiling (height (Curve h t))::Int))) ++ (repString l) ++ "\n</g>\n</svg>"
+
+repString :: [Point] -> String
+repString []     = ""
+repString (p1:p2:ps) = (printf "\n<line style=\"stroke-width: 2px; stroke: black; fill:white\" x1=\"%.2f\" x2=\"%.2f\" y1=\"%.2f\" y2=\"%.2f\" />" ((pointX p1)::Double) ((pointX p2)::Double) ((pointY p1)::Double) ((pointY p2)::Double)) ++ (repString (p2:ps))
+repString p      = ""
+
+toFile :: Curve -> FilePath -> IO ()
+toFile c f = writeFile f (toSVG c)
+
+hilbert :: Curve -> Curve
+hilbert c = c0 `connect` c1 `connect` c2 `connect` c3
+   where  w = width c
+          h = height c
+          p = 6
+
+          ch = reflect c $ Vertical 0
+
+          c0 = ch `rotate` (-90) `translate` point (w+p+w, h+p+h)
+          c1 = c `translate` point (w+p+w, h)
+          c2 = c
+          c3 = ch `rotate` 90 `translate` point (0, h+p)
+
+hilbCom = hilbert $ hilbert $ hilbert $ hilbert $ curve (point (0,0)) []
+
+hilbN :: Int -> Curve
+hilbN 0 = hilbert $ curve (point (0,0)) []
+hilbN n = hilbert $ hilbN (n-1)
