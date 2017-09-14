f6543759e6c35aa40f95fa03a16413298017d2ec
Tom Jager <tj15299@my.bristol.ac.uk>
2017-09-14 17:06:20 +0200

    Boshed the fuck out of assignment 1

--- assignment1/src/SubsInterpreter.hs
+++ assignment1/src/SubsInterpreter.hs
@@ -33,42 +33,94 @@ type Context = (Env, PEnv)
 initialContext :: Context
 initialContext = (Map.empty, initialPEnv)
   where initialPEnv =
-          Map.fromList [ ("===", undefined)
-                       , ("<", undefined)
-                       , ("+", undefined)
-                       , ("*", undefined)
-                       , ("-", undefined)
-                       , ("%", undefined)
+          Map.fromList [ ("===", strictEquals)
+                       , ("<", lessThan)
+                       , ("+", plus)
+                       , ("*", mult)
+                       , ("-", sub)
+                       , ("%", modulus)
                        , ("Array", mkArray)
                        ]
 
-newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
+strictEquals :: Primitive
+strictEquals [IntVal i1,IntVal i2] | i1 == i2          = Right TrueVal
+strictEquals [UndefinedVal,_]                          = Right FalseVal
+strictEquals [TrueVal,TrueVal]                         = Right TrueVal
+strictEquals [FalseVal,FalseVal]                       = Right TrueVal
+strictEquals [StringVal s1,StringVal s2] | s1 == s2    = Right TrueVal
+strictEquals [ArrayVal vs1, ArrayVal vs2] | vs1 == vs2 = Right TrueVal
+strictEquals [_,_]                                     = Right FalseVal
+strictEquals _                                         = Left "=== called with wrong number of arguments"
 
-instance Functor SubsM where
-  fmap = undefined
+lessThan :: Primitive
+lessThan [IntVal i1,IntVal i2] | i1 < i2       = Right TrueVal
+lessThan [StringVal s1,StringVal s2] | s1 < s2 = Right TrueVal
+lessThan [_,_]                                 = Right FalseVal
+lessThan _                                     = Left "< called with incorrect number of arguments"
 
-instance Applicative SubsM where
-  pure = undefined
-  (<*>) = undefined
+plus :: Primitive
+plus [IntVal i1, IntVal i2]      = Right (IntVal (i1+i2))
+plus [StringVal s1,StringVal s2] = Right (StringVal (s1++s2))
+plus [StringVal s,IntVal i]      = Right (StringVal (s ++ (show i)))
+plus [IntVal i, StringVal s]     = Right (StringVal ((show i) ++ s))
+plus [_,_]                       = Left "Only Strings and Integers may be added"
+plus _                           = Left "+ called with incorrect number of arguments"
 
-instance Monad SubsM where
-  return x = undefined 
-  f >>= m = undefined
-  fail s = undefined
+mult :: Primitive
+mult [IntVal a, IntVal b] = Right (IntVal (a*b))
+mult [_, _]              = Left "Only Integers may be multiplied"
+mult _                   = Left "* called with the wrong number of arguments"
 
+sub :: Primitive
+sub [IntVal i1, IntVal i2] = Right (IntVal (i1-i2))
+sub [_,_]                  = Left "Only Integers may be subtracted"
+sub _                      = Left "- called with incorrect number of arguments"
+
+modulus :: Primitive
+modulus [IntVal a, IntVal b] = Right (IntVal (a `mod` b))
+modulus [_, _]               = Left "only Integers may be taken %"
+modulus _                    = Left "wrong number of arguments applied to %"
 
 mkArray :: Primitive
 mkArray [IntVal n] | n >= 0 = return $ ArrayVal (replicate n UndefinedVal)
 mkArray _ = Left "Array() called with wrong number or type of arguments"
 
+
+
+newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
+
+instance Functor SubsM where
+  fmap f fra = SubsM (\c -> case runSubsM fra c of
+    (Left e)        -> Left e
+    (Right (a,env)) -> Right (f a,env))
+
+instance Applicative SubsM where
+  pure a = return a
+  ff <*> fa = SubsM (\c0 -> case runSubsM ff c0 of
+    (Left e)         -> Left e
+    (Right (f, env)) -> case runSubsM fa (env,snd c0) of
+      (Left e')         -> Left e'
+      (Right (a, env')) -> Right (f a, env'))
+
+instance Monad SubsM where
+  return x = SubsM (\c -> Right (x, fst c))
+  m >>= f  = SubsM (\c0 -> case runSubsM m c0 of
+     (Left e)        -> Left e
+     (Right (x,env)) -> case runSubsM (f x) (env,snd c0) of
+       (Left e')         -> Left e'
+       (Right (x',env')) -> Right (x',env'))
+  fail s = error s
+
 modifyEnv :: (Env -> Env) -> SubsM ()
-modifyEnv f = undefined
+modifyEnv f = SubsM (\c -> Right ((),f (fst c)))
 
 putVar :: Ident -> Value -> SubsM ()
-putVar name val = undefined
+putVar name val = modifyEnv (Map.insert name val)
 
 getVar :: Ident -> SubsM Value
-getVar name = undefined
+getVar name = SubsM (\c -> case Map.lookup name (fst c) of
+  Nothing -> Left "Variable not initialised"
+  (Just x)  -> Right (x,fst c))
 
 getFunction :: FunName -> SubsM Primitive
 getFunction name = undefined
